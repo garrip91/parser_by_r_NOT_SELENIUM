f1aa2859326fa2688b953a281c8211e4ce2b7a4b
garrip91 <45920127+garrip91@users.noreply.github.com>
2021-09-29 23:12:14 +0300

    Update test.py

--- src/test.py
+++ src/test.py
@@ -11,9 +11,9 @@ with open("products.txt", "r", encoding="UTF-8") as f:
         if line == "\n":
             continue
         else:
+            line = line.replace('\n', '')
             list_for_products_count.append(line)
     #print(len(list_for_products_count))
-    print(list_for_products_count)
     
 # list_for_data_count = []    
 # with open("data.txt", "r", encoding="UTF-8") as f:
@@ -25,11 +25,11 @@ with open("products.txt", "r", encoding="UTF-8") as f:
     # print(len(list_for_data_count))
     
     
-# list_for_data_count = []    
-# with open("data.txt", "r", encoding="UTF-8") as f:
-    # for line in f.readlines():
-        # if line == "\n":
-            # continue
-        # else:
-            # list_for_data_count.append(line)
-    # print(len(set(list_for_data_count)))
+list_for_data_count = []    
+with open("data.txt", "r", encoding="UTF-8") as f:
+    for line in f.readlines():
+        if line == "\n":
+            continue
+        else:
+            list_for_data_count.append(line)
+    print(len(set(list_for_data_count)))
