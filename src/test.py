683bcf2aa8e6776acd79a5b59d9af128e14e3faf
garrip91 <45920127+garrip91@users.noreply.github.com>
2021-09-30 01:57:58 +0300

    Update test.py

--- src/test.py
+++ src/test.py
@@ -16,28 +16,25 @@ import json
     #print(len(list_for_products_count))
         
 with open("data.txt", "r", encoding="UTF-8") as f:
-    list_for_data_count = []
+    set_for_data_count = set()
     for line in f.readlines():
         if line == "\n":
             continue
         else:
             line = line.split(' || ')
-            line = tuple(line)
-            list_for_data_count.append(line)
-    list_for_data_count = set(list_for_data_count)
-    list_for_data_count = list(list_for_data_count)
-    print(list_for_data_count[0])
+            set_for_data_count.add(line)
+    print(set_for_data_count[:5])
 
-    list_for_data_articles_count = []
-    for i in list_for_data_count:
-        list_for_data_articles_count.append(i[2])
-    list_for_data_articles_count = set(list_for_data_articles_count)
-    list_for_data_articles_count = list(list_for_data_articles_count)
-    #print(list(list_for_data_articles_count)[0])
+    # list_for_data_articles_count = []
+    # for i in list_for_data_count:
+        # list_for_data_articles_count.append(i[2])
+    # list_for_data_articles_count = set(list_for_data_articles_count)
+    # list_for_data_articles_count = list(list_for_data_articles_count)
+    # #print(list(list_for_data_articles_count)[0])
     
-    list_for_NEW_data_count = [[None, None, None, None, None]]
-    for i in list_for_data_count:
-        if i[2] in list_for_data_articles_count:
-            if list_for_NEW_data_count[-1][2] != i[2]:
-                list_for_NEW_data_count.append(i)
-    print(len(list_for_NEW_data_count))
+    # list_for_NEW_data_count = [[None, None, None, None, None]]
+    # for i in list_for_data_count:
+        # if i[2] in list_for_data_articles_count:
+            # if list_for_NEW_data_count[-1][2] != i[2]:
+                # list_for_NEW_data_count.append(i)
+    # print(len(list_for_NEW_data_count))
