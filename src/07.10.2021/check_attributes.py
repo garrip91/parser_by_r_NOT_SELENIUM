68ba2a360579b22e54ee95024d886741f93711dc
garrip91 <45920127+garrip91@users.noreply.github.com>
2021-10-09 02:10:03 +0300

    Summary

--- src/07.10.2021/check_attributes.py
+++ src/07.10.2021/check_attributes.py
@@ -1,7 +1,14 @@
 import json
 
 
-with open('products.json', 'r', encoding='UTF-8') as f:
+#with open('products.json', 'r', encoding='UTF-8') as f:
+with open('products_test.json', 'r', encoding='UTF-8') as f:
     data = json.load(f)
-    for k, v in data[0].items():
-        print(v["Страна производства"])
+    for line in data:
+        for k, v in line.items():
+            if k == "attributes":
+                for attrs_k, attrs_v in v.items():
+                    if attrs_k == "Страна производства":
+                        print(attrs_v)
+                    else:
+                        continue
